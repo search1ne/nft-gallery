27a14c1754ee62e041b44463ce358a85ae057d8f
search1ne <114374119+search1ne@users.noreply.github.com>
2022-11-08 00:09:58 -0800

    resize next icon svg

--- pages/index.jsx
+++ pages/index.jsx
@@ -75,7 +75,7 @@ const Home = () => {
         </div>
 
         <div className='flex justify-end mb-4'>
-        <svg onClick={handleChangePage} className='w-1/6' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg>
+        <svg onClick={handleChangePage} className='w-1/6' width="120" height="120" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z" /></svg>
         </div>
 
         <div className='flex flex-wrap gap-y-12 mt-4 w-5/6 gap-x-2 justify-center'>
